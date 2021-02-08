840246781cffa1c0e8e8f64af197b619e4fcb318
Shailendra Prasad <n.shailendraprasad@gmail.com>
2021-02-08 11:20:26 +0530

    Add project files.

---
+++ DS/Arrays/StringManipulation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS.Arrays
+{
+    public class StringManipulation
+    {
+        public void ReverseString(string s)
+        {
+            string rev = null;
+            var array = s.ToCharArray();
+            for(int i  = array.Length; i > 0; i--)
+            {
+                rev += array[i - 1];
+            }
+        }
+    }
+}
---
+++ DS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays.StringManipulation a = new Arrays.StringManipulation();
+            a.ReverseString("abc");
+        }
+    }
+}
