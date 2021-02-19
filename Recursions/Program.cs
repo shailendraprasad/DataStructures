fe36f56aa392e750bd4c887b4ba6d11396143a8e
shailendraprasad <n.shailendraprasad@gmail.com>
2021-02-19 19:00:42 +0530

    Graphs and Recursions

---
+++ Recursions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Recursions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine($"Factorial is {ClaulateFactorial(6)}");
+           Console.WriteLine($"Fibonacci Sum is {CalculateFibonacci(9)}");
+        }
+
+        static int ClaulateFactorial(int n)
+        {
+            if(n == 2)
+            {
+                return 2;
+            }
+            var x = n * ClaulateFactorial(n - 1);
+            return x;
+        }
+
+        static int CalculateFibonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2);
+            }
+        }
+    }
+}
