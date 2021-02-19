be7e22640295d28e6dab05c0165a16b6917ee1f7
shailendraprasad <n.shailendraprasad@gmail.com>
2021-02-19 19:43:31 +0530

    Selection Sort

--- Sorting/Sorting.cs
+++ Sorting/Sorting.cs
@@ -10,8 +10,9 @@ namespace Sorting
         static void Main(string[] args)
         {
             Console.WriteLine("Sorting");
-            int [] arr = { 21, 9, 6, 67, 0, 4, 3, 2 };
+            int [] arr = { 21, 9, 6, 67, 0, 4, 3, 2, -1 };
             BubbleSort(arr);
+            SelectionSort(arr);
         }
 
         /// <summary>
@@ -38,6 +39,31 @@ namespace Sorting
             Console.WriteLine("[{0}]", string.Join(", ", a));
         }
 
-       
+        /// <summary>
+        /// Selection Sort
+        /// Find the minimum, check and swap with the lowest element in the array
+        /// </summary>
+        /// <param name="a"></param>
+       public static void SelectionSort(int[] a)
+        {
+            int min = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    if (a[j] < min)
+                    {
+                        min = j;
+                    }  
+                }
+
+                var temp = a[0];
+                a[0] = a[min];
+                a[min] = temp;
+
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", a));
+        }
     }
 }
